78e25f8133d351124bbd1b64a578b4a022018b09
Timothy Otieno <timothyking094@gmail.com>
2020-04-14 18:35:03 +0300

    Modified the App

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,14 @@
 /* eslint-disable operator-linebreak */
 /* eslint-disable wrap-iife */
+
+const makeWholeNumber = (num) => {
+  const str = String(num);
+  if (str.indexOf('.') < 0) {
+    return Number(str);
+  }
+  return Number(str.slice(0, str.indexOf('.')));
+};
+
 const covid19ImpactEstimator = (data) => {
   const {
     region,
@@ -15,60 +24,60 @@ const covid19ImpactEstimator = (data) => {
 
   switch (periodType) {
     case 'days':
-      days = Math.trunc(timeToElapse);
+      days = makeWholeNumber(timeToElapse);
       break;
     case 'weeks':
-      days = Math.trunc(timeToElapse) * 7;
+      days = makeWholeNumber(timeToElapse) * 7;
       break;
     case 'months':
-      days = Math.trunc(timeToElapse) * 30;
+      days = makeWholeNumber(timeToElapse) * 30;
       break;
     default:
-      days = Math.trunc(timeToElapse);
+      days = makeWholeNumber(timeToElapse);
       break;
   }
 
   const impact = (function impactValue() {
     /** Challenge 1 Impact */
     // Currently Infected
-    const currentlyInfected = Math.trunc(reportedCases) * 10;
+    const currentlyInfected = makeWholeNumber(reportedCases) * 10;
 
     // Number of Infected people in a duration of time
     let infectionsByRequestedTime = (function infectionPerTime() {
-      const exponent = Math.trunc(days / 3);
+      const exponent = makeWholeNumber(days / 3);
       return currentlyInfected * 2 ** exponent;
     })();
 
-    infectionsByRequestedTime = Math.trunc(infectionsByRequestedTime);
+    infectionsByRequestedTime = makeWholeNumber(infectionsByRequestedTime);
 
     /** Challenge 2 Impact */
     // Severe Cases in a duration of time
-    const severeCasesByRequestedTime = Math.trunc(
+    const severeCasesByRequestedTime = makeWholeNumber(
       0.15 * infectionsByRequestedTime
     );
 
     // Number of Available Beds
     let hospitalBedsByRequestedTime = (function availableBeds() {
-      const bedsAvailable = Math.trunc(0.35 * totalHospitalBeds);
+      const bedsAvailable = makeWholeNumber(0.35 * totalHospitalBeds);
       return bedsAvailable - severeCasesByRequestedTime;
     })();
 
-    hospitalBedsByRequestedTime = Math.trunc(hospitalBedsByRequestedTime);
+    hospitalBedsByRequestedTime = makeWholeNumber(hospitalBedsByRequestedTime);
 
     /** Challenge 3 Impact */
     // Cases that require ICU
-    const casesForICUByRequestedTime = Math.trunc(
+    const casesForICUByRequestedTime = makeWholeNumber(
       0.05 * infectionsByRequestedTime
     );
 
     // Cases that require Ventilators
-    const casesForVentilatorsByRequestedTime = Math.trunc(
+    const casesForVentilatorsByRequestedTime = makeWholeNumber(
       0.02 * infectionsByRequestedTime
     );
 
     // Money Lost by the Economy Daily
-    const dollarsInFlight = Math.trunc(
-      Math.trunc(
+    const dollarsInFlight = makeWholeNumber(
+      makeWholeNumber(
         infectionsByRequestedTime *
           avgDailyIncomePopulation *
           avgDailyIncomeInUSD
@@ -89,44 +98,44 @@ const covid19ImpactEstimator = (data) => {
   const severeImpact = (function severeImpactValue() {
     /** Challenge 1 Severe Impact */
     // Currently Infected
-    const currentlyInfected = Math.trunc(reportedCases) * 50;
+    const currentlyInfected = makeWholeNumber(reportedCases) * 50;
 
     // Number of Infected people in a duration of time
     let infectionsByRequestedTime = (function infectionPerTime() {
-      const exponent = Math.trunc(days / 3);
+      const exponent = makeWholeNumber(days / 3);
       return currentlyInfected * 2 ** exponent;
     })();
 
-    infectionsByRequestedTime = Math.trunc(infectionsByRequestedTime);
+    infectionsByRequestedTime = makeWholeNumber(infectionsByRequestedTime);
 
     /** Challenge 2 Severe Impact */
     // Severe Cases in a duration of time
-    const severeCasesByRequestedTime = Math.trunc(
+    const severeCasesByRequestedTime = makeWholeNumber(
       0.15 * infectionsByRequestedTime
     );
 
     // Number of Available Beds
     let hospitalBedsByRequestedTime = (function availableBeds() {
-      const bedsAvailable = Math.trunc(0.35 * totalHospitalBeds);
+      const bedsAvailable = makeWholeNumber(0.35 * totalHospitalBeds);
       return bedsAvailable - severeCasesByRequestedTime;
     })();
 
-    hospitalBedsByRequestedTime = Math.trunc(hospitalBedsByRequestedTime);
+    hospitalBedsByRequestedTime = makeWholeNumber(hospitalBedsByRequestedTime);
 
     /** Challenge 3 Severe Impact */
     // Cases that require ICU
-    const casesForICUByRequestedTime = Math.trunc(
+    const casesForICUByRequestedTime = makeWholeNumber(
       0.05 * infectionsByRequestedTime
     );
 
     // Cases that require Ventilators
-    const casesForVentilatorsByRequestedTime = Math.trunc(
+    const casesForVentilatorsByRequestedTime = makeWholeNumber(
       0.02 * infectionsByRequestedTime
     );
 
     // Money Lost by the Economy Daily
-    const dollarsInFlight = Math.trunc(
-      Math.trunc(
+    const dollarsInFlight = makeWholeNumber(
+      makeWholeNumber(
         infectionsByRequestedTime *
           avgDailyIncomePopulation *
           avgDailyIncomeInUSD
