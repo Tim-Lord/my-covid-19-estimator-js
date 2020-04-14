114db53fbe7ea15630f205f0da724c36786afc45
Timothy Otieno <timothyking094@gmail.com>
2020-04-14 12:35:33 +0300

    Added the impact and severeImpact variables and assigned their respective values

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,20 @@
-const covid19ImpactEstimator = (data) => data;
+import estimator from './covidEstimator';
+
+const covid19ImpactEstimator = (data) => {
+  // Initialise the function
+  const value = estimator(data);
+
+  // Get values for the Impact
+  const impact = value.impact();
+
+  // Get values for Severe Impact
+  const severeImpact = value.severeImpact();
+
+  return {
+    data,
+    impact,
+    severeImpact
+  };
+};
 
 export default covid19ImpactEstimator;
